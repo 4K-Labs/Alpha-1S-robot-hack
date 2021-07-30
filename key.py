d18ba29c0a50f0b0c8535249d5420b4f8da54a37
Abenezer Kebede <73950771+abeni5049@users.noreply.github.com>
2021-07-30 09:43:03 +0300

    Update key.py

--- key.py
+++ key.py
@@ -1,15 +1,10 @@
 import keyboard
 import lib
-address = "88:1B:99:06:d1:54"
+address = "88:1B:99:09:E8:96"
 lib.connect(address)
-keys = { 'w':lib.moveForward, 's':lib.moveBackwardFast , 'a':lib.moveLeftFast , 'd':lib.moveRightFast , 'v':lib.turnLeft2 , 'm':lib.turnRight2 , 'h':lib.defaultPose}
+keys = { 'w':lib.moveForward, 's':lib.moveBackwardFast , 'a':lib.moveLeft , 'd':lib.moveRightFast , 'v':lib.turnLeft2 , 'm':lib.turnRight2 , 'h':lib.defaultPose}
 while True:
-        key = keyboard.read_key()
-        print(key)
-        try:
-            if keys.get(key):
-                keys.get(key)()
-        except:
-            pass
-         
+    key = keyboard.read_key()
+    if keys.get(key):
+        keys.get(key)()
         
